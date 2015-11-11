3c9f70361b19841709ef75c6085bc8fb3d2d35f8
kubamarchwicki <kuba@marchwicki.pl>
2015-11-11 21:42:00 +0100

    Arquillian Cube Containerless - add logging

--- arquillian-cube-compose/src/test/java/com/example/TranslationTest.java
+++ arquillian-cube-compose/src/test/java/com/example/TranslationTest.java
@@ -13,6 +13,8 @@ import org.junit.runner.RunWith;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static com.jayway.restassured.RestAssured.get;
 import static com.jayway.restassured.path.json.JsonPath.from;
@@ -23,6 +25,8 @@ import static org.junit.Assert.assertThat;
 @RunWith(Arquillian.class)
 public class TranslationTest {
 
+    private static final Logger log = Logger.getLogger(TranslationTest.class.getName());
+
 //  https://docs.jboss.org/author/display/ARQGRA2/Framework+Integration+Options
 //    Standalone
 //    * runs tests without container integration, only lifecycle of extensions is managed
@@ -31,9 +35,16 @@ public class TranslationTest {
 //    * runs tests with container, managed lifecycle of container including deployment
 //    * you can still use Graphene without managing lifecycle - just do not provide @Deployment in your test case
 
+    @Deployment(testable = false)
+    public static GenericArchive createDeployment() {
+        return ShrinkWrap.create(GenericArchive.class, "app.tar");
+    }
+
     @Test
     @RunAsClient
     public void shouldReturnMessage() throws IOException, InterruptedException {
+        log.log(Level.INFO, "Foo");
+
         String json = get(new URL("http://localhost:18080/translate/domek")).asString();
         List<String> translations = from(json).get("englishWord");
 
